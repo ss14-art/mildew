652890f451b4c1348a4b1509857d951852379a8e
mirrorcult <mirrorcult@pm.me>
2026-01-03 23:35:59 +0000

    Fix viewcone effects (#763)

--- Content.Trauma.Shared/Viewcone/Components/ViewconeOccludableComponent.cs
+++ Content.Trauma.Shared/Viewcone/Components/ViewconeOccludableComponent.cs
@@ -6,7 +6,7 @@ using Robust.Shared.Physics;
 namespace Content.Trauma.Shared.Viewcone.Components;
 
 /// <summary>
-///     Marks an entity as one which should fade away clientside if you have a viewcone and it's out of view
+/// Marks an entity as one which should fade away clientside if you have a viewcone and it's out of view
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class ViewconeOccludableComponent : Component, IComponentTreeEntry<ViewconeOccludableComponent>
@@ -15,22 +15,22 @@ public sealed partial class ViewconeOccludableComponent : Component, IComponentT
     public bool OccludeIfAnchored = false;
 
     /// <summary>
-    ///     Whether the occluding should be inverted,
-    ///     i.e. the sprite will be invisible while within view, and visible outside of view
+    /// Whether the occluding should be inverted,
+    /// i.e. the sprite will be invisible while within view, and visible outside of view
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool Inverted = false;
 
     /// <summary>
-    ///     If true, viewcone alpha handling will always override the base alpha of this entity when setting transparency.
-    ///     Useful for viewcone effects.
+    /// If true, viewcone alpha handling will always override the base alpha of this entity when setting transparency.
+    /// Useful for viewcone effects.
     /// </summary>
-    [DataField, AutoNetworkedField]
-    public bool OverrideBaseAlpha = false;
+    [DataField]
+    public bool OverrideBaseAlpha;
 
     /// <summary>
-    ///     If this is a temporary entity (like an effect), then this is the originating player (or other source)
-    ///     of this occludable.
+    /// If this is a temporary entity (like an effect), then this is the originating player (or other source)
+    /// of this occludable.
     /// </summary>
     [DataField, AutoNetworkedField]
     public EntityUid? Source = null;
