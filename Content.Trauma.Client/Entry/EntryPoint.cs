fc0c0dfa8b209a1edfe748b809e8fedb77a9e8f3
deltanedas <@deltanedas:kde.org>
2026-05-12 16:28:36 +0100

    move keybind registry to trauma.client

---
+++ Content.Trauma.Client/Entry/EntryPoint.cs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.UserInterface;
+using Content.Trauma.Client.Input;
+using Content.Trauma.Client.IoC;
+using Content.Trauma.Client.ItemSlotRenderer;
+using Content.Trauma.Client.UserInterface;
+using Robust.Client.Graphics;
+using Robust.Client.Input;
+using Robust.Shared.ContentPack;
+
+namespace Content.Trauma.Client.Entry;
+
+public sealed class EntryPoint : GameClient
+{
+    [Dependency] private readonly IInputManager _input = default!;
+    [Dependency] private readonly IOverlayManager _overlay = default!;
+
+    public override void PreInit()
+    {
+        base.PreInit();
+
+        ContentTraumaClientIoC.Register(Dependencies);
+        IoCManager.InjectDependencies(this);
+    }
+
+    public override void PostInit()
+    {
+        base.PostInit();
+
+        TraumaInputContexts.SetupContexts(_input.Contexts);
+
+        _overlay.AddOverlay(new SpriteToLayerBullshitOverlay());
+    }
+}
---
+++ Content.Trauma.Common/Input/TraumaKeyFunctions.cs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.Input;
+
+namespace Content.Trauma.Common.Input;
+
+[KeyFunctions]
+public static class TraumaKeyFunctions
+{
+    public static readonly BoundKeyFunction OpenMartialArtsMenu = "OpenMartialArtsMenu";
+    public static readonly BoundKeyFunction Tackle = "Tackle";
+    public static readonly BoundKeyFunction FocusCollectiveMindChat = "FocusCollectiveMindChatWindow";
+    public static readonly BoundKeyFunction OpenLanguageMenu = "OpenLanguageMenu";
+    public static readonly BoundKeyFunction ResistGrab = "ResistGrab";
+    public static readonly BoundKeyFunction NetBandwidth = "NetBandwidth";
+
+    // Targeting
+    public static readonly BoundKeyFunction TargetHead = "TargetHead";
+    public static readonly BoundKeyFunction TargetChest = "TargetChest";
+    public static readonly BoundKeyFunction TargetGroin = "TargetGroin";
+    public static readonly BoundKeyFunction TargetLeftArm = "TargetLeftArm";
+    public static readonly BoundKeyFunction TargetLeftHand = "TargetLeftHand";
+    public static readonly BoundKeyFunction TargetRightArm = "TargetRightArm";
+    public static readonly BoundKeyFunction TargetRightHand = "TargetRightHand";
+    public static readonly BoundKeyFunction TargetLeftLeg = "TargetLeftLeg";
+    public static readonly BoundKeyFunction TargetLeftFoot = "TargetLeftFoot";
+    public static readonly BoundKeyFunction TargetRightLeg = "TargetRightLeg";
+    public static readonly BoundKeyFunction TargetRightFoot = "TargetRightFoot";
+}
